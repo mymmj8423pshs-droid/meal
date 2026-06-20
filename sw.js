cf8763e69f89b378e1ccf7ff4e56d24b2ee6f583
mymmj8423pshs-droid <mymmj8423pshs@gmail.com>
2026-06-20 19:46:04 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+// 급식 알리미 서비스워커
+const CACHE = 'meal-app-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png',
+  './apple-touch-icon.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).catch(() => {}));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
+  // 나이스 API/Firebase 등 외부 요청은 항상 네트워크로 (캐시하지 않음)
+  if (req.method !== 'GET' || new URL(req.url).origin !== self.location.origin) {
+    return;
+  }
+  // 앱 셸: 네트워크 우선, 실패 시 캐시
+  e.respondWith(
+    fetch(req)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(req, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(req).then((r) => r || caches.match('./index.html')))
+  );
+});
